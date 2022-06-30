dd657d688ae6b0677cdbe2cfda5db56fda5b9799
Reynaldi <reynaldi@teras.work>
2022-07-01 00:33:08 +0700

    Add Day-4 Resource

---
+++ day-4/ownership/target/debug/deps/ownership-ad851afaec5c0b08.d
@@ -0,0 +1,5 @@
+/Users/reynaldi/IdeaProjects/learn-rust/day-4/ownership/target/debug/deps/ownership-ad851afaec5c0b08: src/main.rs
+
+/Users/reynaldi/IdeaProjects/learn-rust/day-4/ownership/target/debug/deps/ownership-ad851afaec5c0b08.d: src/main.rs
+
+src/main.rs:
